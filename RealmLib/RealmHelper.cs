19ac8764ed45b599bba5448e1029ae56c4dbb3a1
hiroaki.yoshii <orient110yh@gmail.com>
2019-05-26 23:24:05 +0900

    RealmHelper 実装

---
+++ RealmLib/RealmHelper.cs
@@ -0,0 +1,54 @@
+﻿using Realms;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RealmLib
+{
+    public class RealmHelper
+    {
+        public static void RemoveCascade(RealmObject entity, Realm db)
+        {
+            var toBeDeleted = new List<RealmObject>();
+            toBeDeleted.Add(entity);
+            while (toBeDeleted.Count > 0)
+            {
+                var element = toBeDeleted[0];
+                toBeDeleted.RemoveAt(0);
+                Resolve(element, toBeDeleted);
+                db.Remove(element);
+            }
+        }
+
+        public static void Resolve(RealmObject element, List<RealmObject> toBeDeleted)
+        {
+            foreach (var property in element.GetType().GetProperties())
+            {
+                if (IsEntity(property.PropertyType))
+                {
+                    toBeDeleted.Add((RealmObject)property.GetValue(element));
+                }
+                if (IsSubEntity(property.PropertyType))
+                {
+                    foreach (var value in property.GetValue(element) as IList)
+                    {
+                        toBeDeleted.Add(value as RealmObject);
+                    }
+                }
+            }
+        }
+
+        public static bool IsEntity(Type type)
+        {
+            return type.IsSubclassOf(typeof(RealmObject));
+        }
+
+        public static bool IsSubEntity(Type type)
+        {
+            return type.IsGenericType
+                && type.GetGenericTypeDefinition() == typeof(List<>)
+                && type.GetGenericArguments()[0].IsSubclassOf(typeof(RealmObject));
+        }
+
+    }
+}
---
+++ RealmLibTests/RealmHelperTests.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RealmLib;
+using Realms;
+using System;
+using System.Collections.Generic;
+
+namespace RealmLibTests
+{
+    [TestClass]
+    public class RealmHelperTests
+    {
+        [TestMethod]
+        public void IsEntity()
+        {
+            Assert.IsTrue(RealmHelper.IsEntity(new BaseEntity1().GetType()));
+            Assert.IsTrue(RealmHelper.IsEntity(new SubEntity1().GetType()));
+            Assert.IsTrue(RealmHelper.IsEntity(new SubEntity2().GetType()));
+            Assert.IsTrue(RealmHelper.IsEntity(new SubSubEntity1().GetType()));
+            Assert.IsFalse(RealmHelper.IsEntity(new NotEntity().GetType()));
+        }
+        [TestMethod]
+        public void IsSubEntity()
+        {
+            Assert.IsFalse(RealmHelper.IsSubEntity(new BaseEntity1().GetType()));
+            Assert.IsFalse(RealmHelper.IsSubEntity(new SubEntity1().GetType()));
+            Assert.IsFalse(RealmHelper.IsSubEntity(new SubEntity2().GetType()));
+            Assert.IsFalse(RealmHelper.IsSubEntity(new SubSubEntity1().GetType()));
+            Assert.IsFalse(RealmHelper.IsSubEntity(new NotEntity().GetType()));
+            Assert.IsTrue(RealmHelper.IsSubEntity(new List<SubEntity1>().GetType()));
+            Assert.IsFalse(RealmHelper.IsSubEntity(new List<NotEntity>().GetType()));
+        }
+        [TestMethod]
+        public void resolve()
+        {
+            var entities1 = new List<SubEntity1>();
+            entities1.Add(new SubEntity1());
+            entities1.Add(new SubEntity1());
+            BaseEntity1 element = new BaseEntity1()
+            {
+                sub1 = entities1,
+                sub2 = new SubEntity2(),
+                b1 = 11111
+            };
+
+            var list = new List<RealmObject>();
+            RealmHelper.Resolve(element, list);
+            Assert.AreEqual(3, list.Count);
+        }
+    }
+
+    public class BaseEntity1 : RealmObject
+    {
+        public int b1 { get; set; }
+        public List<SubEntity1> sub1 { get; set; }
+        public NotEntity ne { get; set; }
+        public SubEntity2 sub2 { get; set; }
+    }
+
+    public class SubEntity1 : RealmObject
+    {
+        int s1 { get; set; }
+    }
+
+    public class SubEntity2 : RealmObject
+    {
+        int s2 { get; set; }
+        List<SubEntity1> subSub1 { get; set; }
+    }
+    public class SubSubEntity1 : RealmObject
+    {
+        int ss3 { get; set; }
+    }
+    public class NotEntity
+    {
+        int ne1 { get; set; }
+    }
+
+
+}
+
